97cce82d0d730a8839fd258707f3082981331278
Maxim Samokhvalov <suprocketdev@gmail.com>
2023-09-22 12:53:56 +0300

    fix: cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,4 @@
-import readlineSync from 'readline-sync';
+import readlineSync from "readline-sync";
 
 const getYourName = () => {
   const name = readlineSync.question("Hey! What's your name?: ");
