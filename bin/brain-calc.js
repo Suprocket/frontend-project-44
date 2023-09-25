9c27d80a8e7980bb6aaf707ba18cd6e8a4a2ed82
Maxim Samokhvalov <suprocketdev@gmail.com>
2023-09-25 12:21:01 +0300

    fix: brain-calc.js updated by eslint

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -13,12 +13,14 @@ const calcGame = () => {
   while (correctAnswers < 3) {
     const randomNumber1 = Math.floor(Math.random() * 100);
     const randomNumber2 = Math.floor(Math.random() * 100);
-    const arrOfOperations = ['+','-','/','*'];
-    const randomNumOfOperation = Math.floor(Math.random() * arrOfOperations.length);
-    const randomOperation =  arrOfOperations[randomNumOfOperation];
+    const arrOfOperations = ['+', '-', '/', '*'];
+    const randomNumOfOperation = Math.floor(
+      Math.random() * arrOfOperations.length,
+    );
+    const randomOperation = arrOfOperations[randomNumOfOperation];
     let correctAnswer = 0;
 
-    switch(randomOperation){
+    switch (randomOperation) {
       case '+':
         correctAnswer = randomNumber1 + randomNumber2;
         break;
@@ -31,17 +33,26 @@ const calcGame = () => {
       case '/':
         correctAnswer = Math.floor(randomNumber1 / randomNumber2);
         break;
+      default:
+        console.log('Error in switch/case of randomOperation');
+        break;
     }
 
-    const userAnswer = Number(readlineSync.question(`Question: ${randomNumber1} ${randomOperation} ${randomNumber2} \n`));
+    const userAnswer = Number(
+      readlineSync.question(
+        `Question: ${randomNumber1} ${randomOperation} ${randomNumber2} \n`,
+      ),
+    );
     if (userAnswer === correctAnswer) {
       correctAnswers += 1;
       console.log('Correct!');
     } else {
-        console.log(typeof(correctAnswer));
-        console.log(typeof(userAnswer));
-        console.log(`'${userAnswer}' is wrong answer. Correct answer was '${correctAnswer}'`);
-        break;
+      console.log(typeof correctAnswer);
+      console.log(typeof userAnswer);
+      console.log(
+        `'${userAnswer}' is wrong answer. Correct answer was '${correctAnswer}'`,
+      );
+      break;
     }
   }
   if (correctAnswers === 3) {
