91558fa95c7721011e57a3cbce1ca085301945c9
Maxim Samokhvalov <suprocketdev@gmail.com>
2023-10-05 02:11:55 +0300

    fix: output question for user

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -33,7 +33,7 @@ const playProgression = () => {
 
     const compAnswer = Number(arrOfNumbers.splice(addIndexForPoints, 1, '...'));
 
-    console.log(`Question: ${arrOfNumbers}`);
+    console.log(`Question: ${arrOfNumbers.join(' ')}`);
     const userAnswer = Number(readlineSync.question('Your answer: '));
 
     if (userAnswer === compAnswer) {
