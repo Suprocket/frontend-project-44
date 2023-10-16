19de6cd048cc6ea58bd68751821ed841f73bc22c
Maxim Samokhvalov <suprocketdev@gmail.com>
2023-10-17 01:29:56 +0300

    feat: add gameData to playProgression

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,42 +1,25 @@
 import engine from '../index.js';
-import getRandom from '../utils.js';
+import {getRandom, primeOrNot} from '../modules.js';
 
 const gameRules = 'What number is missing in the progression?';
 
 const playProgression = () => {
-  let answerNum = 0;
 
-  function getRandomInt(min, max) {
-    let minimum = min;
-    let maximum = max;
-    minimum = Math.ceil(minimum);
-    maximum = Math.floor(maximum);
-    return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-  }
-
-  const gameQuestion = () => {
+  const gameData = () => {
+    let randomNumber = getRandom(1, 100);
     const arrOfNumbers = [];
-    let randomNumber = getRandom();
-    const step = Math.floor(Math.random() * 10);
-    const numsInArr = Math.floor(Math.random() * 10) + 5;
-
+    let step = getRandom(1,10);
+    const numsInArr = getRandom(5, 10)
     for (let i = 0; i < numsInArr; i += 1) {
       randomNumber += step;
       arrOfNumbers.push(randomNumber);
     }
-    const addIndexForPoints = getRandomInt(0, arrOfNumbers.length - 1);
-
-    answerNum = Number(arrOfNumbers.splice(addIndexForPoints, 1, '..'));
-    const strOfNumbers = arrOfNumbers.join(' ');
-    return strOfNumbers;
-  };
-
-  const compAnswer = (answer) => {
-    let compans = answer;
-    compans = answerNum;
-    return String(compans);
-  };
-  engine(gameRules, gameQuestion, compAnswer);
+    const addIndexForPoints = getRandom(0, arrOfNumbers.length - 1);
+    const compAnswer = String(arrOfNumbers.splice(addIndexForPoints, 1, '..'));
+    const gameQuestion = arrOfNumbers.join(' ');
+    return [gameQuestion, String(compAnswer)];
+  }
+  engine(gameRules, gameData);
 };
 
 export default playProgression;
