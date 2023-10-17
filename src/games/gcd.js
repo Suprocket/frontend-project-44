5bded7bb425812f768ceef138111e92fadbf948c
Maxim Samokhvalov <suprocketdev@gmail.com>
2023-10-18 00:40:02 +0300

    feat: make easy getNOD(), rename getRamdom folder

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,21 +1,13 @@
 import engine from '../index.js';
-import getRandom from '../modules.js';
+import getRandom from '../utils.js';
 
 const gameRules = 'Find the greatest common divisor of given numbers.';
 
 const getNOD = (num1, num2) => {
-  let number1 = num1;
-  let number2 = num2;
-  let total = 0;
-  while (number1 && number2) {
-    if (number1 > number2) {
-      number1 %= number2;
-    } else {
-      number2 %= number1;
-    }
-    total = number1 + number2;
+  if (!num2) {
+    return num1;
   }
-  return total;
+return getNOD(num2, num1 % num2);
 };
 
 const playGCD = () => {
