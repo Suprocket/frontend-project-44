e5e9d5f2bada97974782cd231a9aa38d0b57c574
Maxim Samokhvalov <suprocketdev@gmail.com>
2023-10-18 11:49:53 +0300

    feat: create calculation function in calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,6 +3,19 @@ import getRandom from '../utils.js';
 
 const gameRules = 'What is the result of the expression?';
 
+const calculation = (num1, operator, num2) => {
+  switch (operator) {
+      case '+':
+        return num1 + num2;
+      case '-':
+        return num1 - num2;
+      case '*':
+        return num1 * num2;
+      default:
+        return false;
+    }
+}
+
 const playCalc = () => {
   const gameData = () => {
     const randomNumber1 = getRandom(0, 100);
@@ -12,22 +25,7 @@ const playCalc = () => {
     const numOfOperation = getRandom(0, arrOfOperations.length - 1);
     const randomOperation = arrOfOperations[numOfOperation];
     const gameQuestion = `${randomNumber1} ${randomOperation} ${randomNumber2}`;
-    let compAnswer = '';
-
-    switch (randomOperation) {
-      case '+':
-        compAnswer = randomNumber1 + randomNumber2;
-        break;
-      case '-':
-        compAnswer = randomNumber1 - randomNumber2;
-        break;
-      case '*':
-        compAnswer = randomNumber1 * randomNumber2;
-        break;
-      default:
-        console.log('Error in switch/case of randomOperation');
-        break;
-    }
+    const compAnswer = calculation(randomNumber1, randomOperation, randomNumber2);
     return [gameQuestion, String(compAnswer)];
   };
   engine(gameRules, gameData);
